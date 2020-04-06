799ac4e4ebd8c2bc410cec5a0af6bbff60918420
W3BDEV3L0PER <sashhha1234321@gmail.com>
2020-04-06 08:39:05 +0500

    Updates

---
+++ precache-manifest.a04a476dec626483dd661805f580a88f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fa0efa860bc72468efdfef440ed742",
+    "url": "/attempt5/index.html"
+  },
+  {
+    "revision": "e7e043e9b5db73b2bab4",
+    "url": "/attempt5/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50212fc2a09257541426",
+    "url": "/attempt5/static/js/2.a2099f1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/attempt5/static/js/2.a2099f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e043e9b5db73b2bab4",
+    "url": "/attempt5/static/js/main.7e305fd7.chunk.js"
+  },
+  {
+    "revision": "f13f3c17690d05fbc79b",
+    "url": "/attempt5/static/js/runtime-main.ef0f8041.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/attempt5/static/media/logo.5d5d9eef.svg"
+  }
+]);
